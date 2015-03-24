fadf0a3d101009f59d31d5c248e43a907a968901
NovusTheory <NovusTheory@users.noreply.github.com>
2015-03-24 10:50:14 -0500

    No longer one big line
    It honestly needed to be fixed

--- html/index.php
+++ html/index.php
@@ -3,4 +3,21 @@ ini_set("session.cookie_httponly", 1);
 session_set_cookie_params(0,"/", NULL, NULL, TRUE);
 session_start();
 ?>
-<!DOCTYPE HTML><html><head><title>Forums Broski</title><link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,400' rel='stylesheet' type='text/css'><link rel="stylesheet"type="text/css"href="/s/main.css"/><meta http-equiv="Content-Type" content="text/html; charset=utf-8" /></head><body><div id="mainbody"><div id="userbox"> Loading... </div><div id="logo">dartziepoo</div><div id="navbar"></div><div id="mainnav"></div><div id="container">~</div></div><script type="text/javascript"src="/s/user.js"></script></body></html>
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <title>Forums Broski</title>
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,400' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet"type="text/css"href="/s/main.css"/><meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  </head>
+  <body>
+    <div id="mainbody">
+      <div id="userbox"> Loading... </div>
+      <div id="logo">dartziepoo</div>
+      <div id="navbar"></div>
+      <div id="mainnav"></div>
+      <div id="container">~</div>
+    </div>
+    <script type="text/javascript"src="/s/user.js"></script>
+  </body>
+</html>
